5874f8ca1f18fd6f1b413b208844e969951b9c2c
Lê Thị Ngọc Thảo <lethingocthaobd1234@gmail.com>
2026-09-13 14:31:56 +0000

    add

---
+++ Lab01_Bai1_Framework/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab01_Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Yêu cầu người dùng nhập họ tên
+            Console.Write("Nhap ho va ten cua ban: ");
+            
+            // Đọc chuỗi họ tên vừa nhập từ bàn phím
+            string hoTen = Console.ReadLine();
+            
+            // Xuất họ tên ra màn hình console
+            Console.WriteLine("Xin chao: " + hoTen);
+            
+            // Tạm dừng màn hình để xem kết quả
+            Console.WriteLine("Nhan pham Enter de thoat...");
+            Console.ReadLine();
+        }
+    }
+}
